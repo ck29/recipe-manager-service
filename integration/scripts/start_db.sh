eb2285963e4ab73dcf1791d1a8d407a97bbf8b0b
Chandan Kumar <chanda.kumar@cgi.com>
2022-07-28 00:37:36 +0200

    Removing unused code, adding schema.

--- integration/scripts/start_db.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -p 9000:8000 amazon/dynamodb-local -jar DynamoDBLocal.jar -dbPath . -sharedDb
--- integration/scripts/start_server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar $PROJECT_DIR/target/recipe-manager-service-0.0.1-SNAPSHOT.jar
