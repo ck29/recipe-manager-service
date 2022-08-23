06c8ece6205ec1d6c89592a9d461adf982121362
Chandan Kumar <chanda.kumar@cgi.com>
2022-08-24 00:38:47 +0200

    Adding setup script and updating samples.

--- setup.bat
+++ setup.bat
@@ -1,5 +1,8 @@
 @echo off
 echo "Setting up database.."
+set _cmd=docker ps -q -f "name=dynamodb-local"
+for /F "tokens=1" %%A in ('%_cmd%') DO docker rm -f %%A
+
 start cmd /k docker run --name dynamodb-local -p 8000:8000 amazon/dynamodb-local -jar DynamoDBLocal.jar -dbPath . -sharedDb
 
 echo "Building application.."
@@ -7,8 +10,13 @@ call mvn clean install
 
 echo "Starting application.."
 start cmd /k java -jar target/recipe-manager-service-0.0.1-SNAPSHOT.jar
+echo "Waiting for application to start.."
+sleep 10
 
-
+echo "Creating table.."
 aws dynamodb create-table --cli-input-json file://data/data_model/recipes.json --endpoint-url http://localhost:8000
-REM curl -d "@/samples/recipie_soup.json" -X POST http://localhost:8000/
+
+echo "Adding data.."
+curl -H "Content-Type: application/json" -d "@data/samples/recipe_pancake.json" -X POST http://localhost:8080/recipe
+curl -H "Content-Type: application/json" -d "@data/samples/recipe_lemonade.json" -X POST http://localhost:8080/recipe
 
